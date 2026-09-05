b7993541f2d13bfc62ab3312c3f1753f81b52cbf
ZUKO-MD <132104247+Neggy5@users.noreply.github.com>
2026-09-05 18:09:02 +0100

    Add files via upload

--- nexstore/token.js
+++ nexstore/token.js
@@ -1,4 +1,4 @@
 module.exports = {
-  BOT_TOKEN: '8938920274:AAEGaEKBX-z8_7tLd1PS5d7GehQs5NjQZHo',  
-  startupPassword: 'quantix'
+  BOT_TOKEN: process.env.BOT_TOKEN || '8938920274:AAEGaEKBX-z8_7tLd1PS5d7GehQs5NjQZHo',
+  startupPassword: process.env.STARTUP_PASSWORD || 'zuko'
 };
